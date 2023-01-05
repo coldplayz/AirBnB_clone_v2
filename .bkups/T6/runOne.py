843c184fc80438ec7c7b33f0ec6d89a3b5987fec
coldplayz <106972092+coldplayz@users.noreply.github.com>
2023-01-05 14:19:13 +0100

    Delete .bkups directory

--- .bkups/T6/runOne.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-""" Console Module """
-from console import HBNBCommand
-import io
-from unittest.mock import patch
-
-with patch('sys.stdout', new=io.StringIO()) as f:
-    HBNBCommand().onecmd('help')
-    print(f.getvalue())
