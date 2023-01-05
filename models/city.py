52530a2f7ed2e92a8da669c51a7f280863a9dc4c
Greenbel <greenbel.chibuike@gmail.com>
2023-01-05 15:27:15 +0100

    Implemented task 8.

--- models/city.py
+++ models/city.py
@@ -3,6 +3,7 @@
 from models.base_model import BaseModel, Base
 from datetime import datetime
 from sqlalchemy import Column, ForeignKey, Integer, String, Numeric, Date
+from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
@@ -14,3 +15,7 @@ class City(BaseModel, Base):
             ForeignKey('states.id', ondelete='CASCADE', onupdate='CASCADE'),
             nullable=False)
     name = Column(String(128), nullable=False)
+
+    # Relationships
+    places = relationship(
+            'Place', backref='cities', cascade="all, delete-orphan")
