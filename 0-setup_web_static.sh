b9a7fb8de57e90feccad5bb21770d3ba601c28ac
Greenbel <greenbel.chibuike@gmail.com>
2023-01-21 20:06:14 +0100

    T0: initial commit.

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+# Sets up my web servers for rhe deployment of web_static.
+
+
+sudo apt-get -y update
+sudo apt-get -y install nginx
+sudo service nginx start
+
+sudo mkdir -p /data/web_static/releases/test
+sudo mkdir -p /data/web_static/shared
+sudo ln -sfr -T /data/web_static/releases/test /data/web_static/current
+
+# Create a basic html file to test
+body='<!DOCTYPE html>
+<html>
+	<head>
+		<title>Intro Page</title>
+	</head>
+		<body>
+			<h1>Welcome to Greenbel homepage!</h1>
+		</body>
+</html>'
+
+sudo chown -R ubuntu:ubuntu /data/
+
+echo "$body" > /data/web_static/releases/test/index.html
+
+# See if the desired location block exists
+location=$(sudo grep "	location ^~ /hbnb_static {" /etc/nginx/sites-available/default)
+
+if [ -z "$location" ]
+then
+
+	# Append a location block for /hbnb_static
+	sudo sed -i '/server_name _/a\\n\tlocation ^~ /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
+fi
+
+sudo service nginx restart
